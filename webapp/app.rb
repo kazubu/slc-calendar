c6a61a683457fd098afd0bce07c7958c28b0dd85
Kazubu <kazubu@jtime.net>
2022-02-12 00:00:38 +0900

    webapp returns on_live if live is ongoing

--- webapp/app.rb
+++ webapp/app.rb
@@ -15,9 +15,11 @@ def event2hash(e)
   hash[:live_url] = e.extended_properties.shared["live_url"] if e.extended_properties && e.extended_properties.shared && e.extended_properties.shared["live_url"]
   hash[:thumbnail_url] = e.extended_properties.shared["thumbnail_url"] if e.extended_properties && e.extended_properties.shared && e.extended_properties.shared["thumbnail_url"]
   hash[:live_ended] = e.extended_properties.shared["live_ended"] if e.extended_properties && e.extended_properties.shared && e.extended_properties.shared["live_ended"]
+  hash[:on_live] = e.extended_properties.shared["on_live"] if e.extended_properties && e.extended_properties.shared && e.extended_properties.shared["on_live"]
   if hash[:live_ended].nil?
     if e.description[-2,2] == '##'
       hash[:live_ended] = "true"
+      hash[:on_live] = "false"
     else
       hash[:live_ended] = "false"
     end
